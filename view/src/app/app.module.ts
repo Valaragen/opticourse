045d16cc9e6a61baf50419e860f5ce00840eed96
Valaragen <rudy.boinnard@gmail.com>
2024-02-04 15:47:33 +0100

    Project format : setup eslint on api Simplify .env variable names Setup sequelize pg for the api Change the quotes lint rule of the view

--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./routes/app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AngularHelpComponent } from "./components/angular-help/angular-help.component";
-import { HomeComponent } from "./components/home/home.component";
+import { AppRoutingModule } from './routes/app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularHelpComponent } from './components/angular-help/angular-help.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
--- view/src/app/components/angular-help/angular-help.component.spec.ts
+++ view/src/app/components/angular-help/angular-help.component.spec.ts
@@ -1,8 +1,8 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AngularHelpComponent } from "./angular-help.component";
+import { AngularHelpComponent } from './angular-help.component';
 
-describe("AngularHelpComponent", () => {
+describe('AngularHelpComponent', () => {
   let component: AngularHelpComponent;
   let fixture: ComponentFixture<AngularHelpComponent>;
 
@@ -17,22 +17,22 @@ describe("AngularHelpComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AngularHelpComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it("should have as title 'my-app'", () => {
+  it('should have as title \'my-app\'', () => {
     const fixture = TestBed.createComponent(AngularHelpComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("my-app");
+    expect(app.title).toEqual('my-app');
   });
 
-  it("should render title", () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AngularHelpComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector(".content span")?.textContent).toContain("my-app app is running!");
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
   });
 });
